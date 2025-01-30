5daf0b8a7e432d3fb9364a1064de274b42d5072a
James Wilkinson <78602661+James-Wilkinson-git@users.noreply.github.com>
2025-01-30 09:10:54 -0500

    Updates

--- index.js
+++ index.js
@@ -25,16 +25,13 @@ const gameIds2025 = db.collection("2025games");
 
 
 app.get("/api/games", async (req, res) => {
-  console.log("Received request for /api/games");
   try {
     // Fetch game IDs from the 2025games collection
     const games2025 = await gameIds2025.find().toArray();
-    console.log(`Fetched ${games2025.length} game IDs from 2025games collection`);
     const gameIds = games2025.map(game => game.id);
 
     // Check MongoDB for existing game IDs
     const existingGames = await gamesCollection.find({ id: { $in: gameIds } }).toArray();
-    console.log(`Found ${existingGames.length} existing games in games collection`);
     const existingGameIds = existingGames.map(game => game.id);
     const newGameIds = gameIds.filter(id => !existingGameIds.includes(id));
     console.log(`Identified ${newGameIds.length} new game IDs to fetch`);
@@ -80,6 +77,10 @@ app.get("/api/games", async (req, res) => {
           game.link
             ?.filter((link) => link.$.type === "boardgamedesigner")
             .map((link) => link.$.value) || [],
+        artist:
+        game.link
+          ?.filter((link) => link.$.type === "boardgameartist")
+          .map((link) => link.$.value) || [],
         publisher:
           game.link
             ?.filter((link) => link.$.type === "boardgamepublisher")
@@ -89,36 +90,29 @@ app.get("/api/games", async (req, res) => {
       // Insert each game into MongoDB as it is fetched
       for (const game of gamesToInsert) {
         await gamesCollection.insertOne(game);
-        console.log(`Inserted game with ID: ${game.id}`);
+        console.log(`https://boardgamegeek.com/boardgame/${game.id}`);
         newGames.push(game);
       }
 
       if (i + 20 < newGameIds.length) {
-        console.log("Waiting for 5.5 seconds before next batch");
         await new Promise((resolve) => setTimeout(resolve, 5500));
       }
     }
 
     // Combine existing and new games
     const allGames = [...existingGames, ...newGames];
-    console.log(`Returning ${allGames.length} games in response`);
-
     res.json(allGames);
   } catch (error) {
-    console.error("Error fetching game details:", error);
     res.status(500).send("Failed to fetch game details");
   }
 });
 
 // New endpoint to return all data from MongoDB
 app.get("/api/all-games", async (req, res) => {
-  console.log("Received request for /api/all-games");
   try {
     const games = await gamesCollection.find().toArray();
-    console.log(`Fetched ${games.length} games from games collection`);
     res.json(games);
   } catch (error) {
-    console.error("Error fetching all games from MongoDB:", error);
     res.status(500).send("Failed to fetch all games");
   }
 });
@@ -130,7 +124,7 @@ app.get('/api/scrape', async (req, res) => {
     let page = 1;
     while (true) {
       console.log(`Scraping page ${page}`);
-      const url = `https://boardgamegeek.com/search/boardgame/page/${page}?advsearch=1&q=&include%5Bdesignerid%5D=&include%5Bpublisherid%5D=&geekitemname=&range%5Byearpublished%5D%5Bmin%5D=2025&range%5Byearpublished%5D%5Bmax%5D=2025&range%5Bminage%5D%5Bmax%5D=&range%5Bnumvoters%5D%5Bmin%5D=&range%5Bnumweights%5D%5Bmin%5D=&range%5Bminplayers%5D%5Bmax%5D=&range%5Bmaxplayers%5D%5Bmin%5D=&range%5Bleastplaytime%5D%5Bmin%5D=&range%5Bplaytime%5D%5Bmax%5D=&floatrange%5Bavgrating%5D%5Bmin%5D=&floatrange%5Bavgrating%5D%5Bmax%5D=&floatrange%5Bavgweight%5D%5Bmin%5D=&floatrange%5Bavgweight%5D%5Bmax%5D=&colfiltertype=&searchuser=BoardGaymesJames&nosubtypes%5B0%5D=boardgameexpansion&playerrangetype=normal&B1=Submit`;
+      const url = `https://boardgamegeek.com/search/boardgame/page/${page}?advsearch=1&q=&include%5Bdesignerid%5D=&include%5Bpublisherid%5D=&geekitemname=&range%5Byearpublished%5D%5Bmin%5D=2025&range%5Byearpublished%5D%5Bmax%5D=2025&range%5Bminage%5D%5Bmax%5D=&range%5Bnumvoters%5D%5Bmin%5D=&range%5Bnumweights%5D%5Bmin%5D=&range%5Bminplayers%5D%5Bmax%5D=&range%5Bmaxplayers%5D%5Bmin%5D=&range%5Bleastplaytime%5D%5Bmin%5D=&range%5Bplaytime%5D%5Bmax%5D=&floatrange%5Bavgrating%5D%5Bmin%5D=&floatrange%5Bavgrating%5D%5Bmax%5D=&floatrange%5Bavgweight%5D%5Bmin%5D=&floatrange%5Bavgweight%5D%5Bmax%5D=&colfiltertype=&searchuser=&nosubtypes%5B0%5D=boardgameexpansion&playerrangetype=normal&B1=Submit`;
       const response = await axios.get(url);
       const gameEntries = response.data;
       if (gameEntries.items.length === 0) {
@@ -144,17 +138,14 @@ app.get('/api/scrape', async (req, res) => {
       const existingGameIds = await gameIds2025.find().toArray().then(games => games.map(game => game.id));
 
       const newGameIds = gameIds.filter(id => !existingGameIds.includes(id));
-      console.log(`Identified ${newGameIds.length} new game IDs to save`);
       // Save new game IDs to MongoDB as they are found
       if (newGameIds.length > 0) {
         console.log(`Saving ${newGameIds.length} new game IDs to MongoDB`);
         await gameIds2025.insertMany(newGameIds.map(id => ({ id })));
       }
-      console.log(`Completed page ${page}`);
       page++;
 
       // Add a delay of 2-5 seconds between requests
-      console.log("Waiting for 2-5 seconds before next request");
       const delayTime = Math.floor(Math.random() * (5000 - 2000 + 1)) + 2000;
       await delay(delayTime);
     }
