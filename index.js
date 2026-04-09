e41feb632b616d8cd4b06ae95a739bf47f019aa1
James Wilkinson <jameserwilk@gmail.com>
2026-04-09 09:09:32 -0400

    Soryt backwards

--- index.js
+++ index.js
@@ -788,7 +788,7 @@ app.get("/api/games/2026", async (req, res) => {
 
     const games = await gamesCollection
       .find(filter)
-      .sort({ id: 1 })
+      .sort({ id: -1 })
       .toArray();
 
     res.json({
